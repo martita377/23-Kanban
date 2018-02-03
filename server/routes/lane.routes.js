b0d9e4798073d6b990ae7a1d7c4a1209a4a40e70
Marta P <mapa.startuj@gmail.com>
2018-02-03 22:36:41 +0100

    new features - exercise 23.4

---
+++ server/routes/lane.routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import * as LaneController from '../controllers/lane.controller';
+
+const router = new Router();
+
+// Get all Lanes
+router.route('/lanes').get(LaneController.getLanes);
+
+// Add a new Lane
+router.route('/lanes').post(LaneController.addLane);
+
+// Delete a lane by laneId
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
+
+// Edit lane's name
+router.route('/lanes/:laneId/name').post(LaneController.editName);
+
+export default router;
